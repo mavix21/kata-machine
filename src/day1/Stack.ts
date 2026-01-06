9889e15ca42dbdb7327ab502723c6cb5fc680eb6
mavix21 <marcevizcarra2109@hotmail.com>
2026-01-06 16:36:48 -0500

    Feat: implement Stack DS

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+  value: T;
+  prev: Node<T> | undefined;
+};
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head: Node<T> | undefined;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    const newNode: Node<T> = {
+      value: item,
+      prev: this.head,
+    };
+
+    this.head = newNode;
+    this.length++;
+  }
+
+  pop(): T | undefined {
+    if (this.head === undefined) {
+      return undefined;
     }
 
-    push(item: T): void {
+    const nodeToBeRemoved = this.head;
+    this.head = nodeToBeRemoved.prev;
+    this.length--;
 
-}
-    pop(): T | undefined {
+    // free
+    nodeToBeRemoved.prev = undefined;
 
-}
-    peek(): T | undefined {
+    return nodeToBeRemoved.value;
+  }
 
-}
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+
+  isEmpty(): boolean {
+    return this.head === undefined || this.length === 0;
+  }
 }
